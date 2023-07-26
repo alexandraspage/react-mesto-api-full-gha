0986328e69bce37abc89e95c945c489f0918d44b
Sasha Dudina <aleksad301@gmail.com>
2023-07-27 01:45:19 +0300

    first

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
